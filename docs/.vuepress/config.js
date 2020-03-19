78d539cc9792a2a8674c23b7074df465ffaf3708
Gaetan SENN <gaetan.senn@gmail.com>
2020-03-19 18:08:11 +0100

    app: Add vuepress config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  title: 'XHR Cache Module',
+  description: 'Cache application/json api resources and serve it as static resource with NuxtJS',
+  themeConfig: {
+    repo: '@dewib/xhr-cache',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    sidebarDepth: 2,
+    sidebar: {
+      '/api/': [
+        '/api/options',
+        '/api/plugin',
+        '/api/methods'
+      ],
+      '/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '/',
+            '/guide/setup',
+            '/guide/resource'
+          ]
+        },
+        {
+          title: 'Resources',
+          collapsable: false,
+          children: [
+            '/resources/default',
+            '/resources/custom'
+          ]
+        }
+      ],
+    },
+    nav: [
+      {
+        text: 'Guide',
+        link: '/'
+      },
+      {
+        text: 'API',
+        link: '/api/'
+      }
+    ]
+  }
+}
